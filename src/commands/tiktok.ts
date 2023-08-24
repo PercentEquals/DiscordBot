e3dba27b649ddc2fe30ba74c1fb0d763b7787d9a
PercentEquals <bartosz.nti@gmail.com>
2023-08-24 20:59:43 +0200

    Added support for audio only download

--- src/commands/tiktok.ts
+++ src/commands/tiktok.ts
@@ -1,10 +1,10 @@
-import { ApplicationCommandType, AttachmentBuilder, Client, CommandInteraction, SlashCommandBooleanOption, SlashCommandStringOption } from "discord.js";
+import { ApplicationCommandType, AttachmentBuilder, Client, CommandInteraction, SlashCommandBooleanOption, SlashCommandStringOption, SlashCommandSubcommandGroupBuilder } from "discord.js";
 import { Command } from "../command";
 import youtubedl from "youtube-dl-exec";
-import fs from 'fs';
+import fs from "fs";
 
-import ffmpegStatic from 'ffmpeg-static';
-import ffmpeg from 'fluent-ffmpeg';
+import ffmpegStatic from "ffmpeg-static";
+import ffmpeg from "fluent-ffmpeg";
 import cheerio from "cheerio";
 
 const DISCORD_LIMIT = 23 * 1024 * 1024; // ~25MB (23 to be sure)
@@ -12,51 +12,61 @@ const DISCORD_LIMIT = 23 * 1024 * 1024; // ~25MB (23 to be sure)
 ffmpeg.setFfmpegPath(ffmpegStatic as string);
 fs.mkdirSync('cache', { recursive: true });
 
-async function convertVideo(id: string) {
+async function convertVideo(id: string, audioOnly: boolean): Promise<string> {
     return new Promise((resolve, reject) => {
+        const initialPath = `cache/${id}.mp4`;
+        const finalPath = `cache/${id}-ffmpeg.${audioOnly ? 'mp3' : 'mp4'}`;
+
         console.log('[ffmpeg] converting', ffmpegStatic);
         
-        const process = ffmpeg(`cache/${id}.mp4`);
+        const process = ffmpeg(initialPath);
         process.videoCodec('libx264');
         process.addOption(["-preset", "ultrafast"]);
-        process.output(`cache/${id}-ffmpeg.mp4`);
+        process.output(finalPath);
         process.on('end', (done: any) => {
             console.log('[ffmpeg] conversion done');
-            resolve(done);
+            resolve(finalPath);
         })
         process.on('error', (err: any) => {
             console.log('[ffmpeg] error', err);
             reject(err);
         })
         
-        if (fs.statSync(`cache/${id}.mp4`).size > DISCORD_LIMIT) {
+        if (fs.statSync(initialPath).size > DISCORD_LIMIT) {
             console.log('[ffmpeg] also compressing');
             process.videoBitrate('300k');
+        }
+
+        if (audioOnly) {
+            process.noVideo();
+            process.audioCodec('copy');
+            process.format('mp3');
         }
 
         process.run();
     })
 }
 
-async function downloadVideo(interaction: CommandInteraction, id: string, url: string, spoiler: boolean) {
+async function downloadVideo(interaction: CommandInteraction, id: string, url: string, spoiler: boolean, audioOnly: boolean) {   
+    const initialPath = `cache/${id}.mp4`;
+
     const result = await youtubedl(url, {
-        format: 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4',
-        output: `cache/${id}.mp4`
+        output: initialPath
     });
 
     console.log(result);
 
-    await convertVideo(id);
+    const finalPath = await convertVideo(id, audioOnly);
 
-    const file = new AttachmentBuilder(`cache/${id}-ffmpeg.mp4`);
-    file.setName(`${id}.mp4`);
+    const file = new AttachmentBuilder(finalPath);
+    file.setName(`${id}.${audioOnly ? 'mp3' : 'mp4'}`);
     file.setSpoiler(spoiler);
 
     console.log('[discord] sending');
 
-    if (fs.statSync(`cache/${id}-ffmpeg.mp4`).size > DISCORD_LIMIT) {
+    if (fs.statSync(finalPath).size > DISCORD_LIMIT) {
         console.log('[discord] file too big');
-        const size = fs.statSync(`cache/${id}-ffmpeg.mp4`).size / 1024 / 1024;
+        const size = fs.statSync(finalPath).size / 1024 / 1024;
         throw new Error(`File too big - ${size}MB / ${DISCORD_LIMIT / 1024 / 1024}MB - ${url}`);
     }
 
@@ -65,11 +75,11 @@ async function downloadVideo(interaction: CommandInteraction, id: string, url: s
         files: [file]
     });
 
-    fs.unlinkSync(`cache/${id}.mp4`);
-    fs.unlinkSync(`cache/${id}-ffmpeg.mp4`);
+    fs.unlinkSync(initialPath);
+    fs.unlinkSync(finalPath);
 }
 
-async function downloadSlideshow(interaction: CommandInteraction, id: string, url: string, spoiler: boolean) {
+async function downloadSlideshow(interaction: CommandInteraction, id: string, url: string, spoiler: boolean, audioOnly: boolean) {
     const response = await fetch(url);
     const body = await response.text();
 
@@ -80,6 +90,11 @@ async function downloadSlideshow(interaction: CommandInteraction, id: string, ur
     const sigi_state = JSON.parse($script.html() as string);
 
     const key = Object.keys(sigi_state.ItemModule)[0];
+
+    if (!sigi_state.ItemModule?.[key]?.imagePost?.images || audioOnly) {
+        return false;
+    }
+
     const imagesData = sigi_state.ItemModule[key].imagePost.images;
 
     imagesData.forEach((image: { imageURL: { urlList: string[] } }, i: number) => {
@@ -101,6 +116,8 @@ async function downloadSlideshow(interaction: CommandInteraction, id: string, ur
         ephemeral: false,
         files
     });
+
+    return true;
 }
 
 export const Tiktok: Command = {
@@ -108,8 +125,9 @@ export const Tiktok: Command = {
     description: "Send tiktok video/slideshow via url",
     type: ApplicationCommandType.ChatInput,
     options: [
-        new SlashCommandStringOption().setName('url').setDescription('tiktok url').setRequired(true),
-        new SlashCommandBooleanOption().setName('spoiler').setDescription('spoiler').setRequired(false)
+        new SlashCommandStringOption().setName('url').setDescription('Tiktok link').setRequired(true),
+        new SlashCommandBooleanOption().setName('spoiler').setDescription('Should hide as spoiler?').setRequired(false),
+        new SlashCommandBooleanOption().setName('audio').setDescription('Should only download audio?').setRequired(false)
     ],
     run: async (client: Client, interaction: CommandInteraction) => {
         try {
@@ -117,6 +135,8 @@ export const Tiktok: Command = {
             const url: string = interaction.options.getString('url', true).split('?')[0];
             //@ts-ignore
             const spoiler = interaction.options.getBoolean('spoiler', false);
+            //@ts-ignore
+            const audio = interaction.options.getBoolean('audio', false);
 
             let id = url.split('/')[url.split('/').length - 1];
             if (id === '') {
@@ -131,14 +151,8 @@ export const Tiktok: Command = {
                 throw new Error('Not a tiktok url');
             }
 
-            try {
-                await downloadSlideshow(interaction, id, url, spoiler);
-            } catch (slideshowError) {
-                try {
-                    await downloadVideo(interaction, id, url, spoiler);
-                } catch (videoError) {
-                    throw new Error(`\nSlideshow error: ${slideshowError}\nVideo error: ${videoError}`);
-                }
+            if (!await downloadSlideshow(interaction, id, url, spoiler, audio)) {
+                await downloadVideo(interaction, id, url, spoiler, audio);
             }
         } catch (e) {
             console.error(e);
